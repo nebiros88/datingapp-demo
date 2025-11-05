4e33a34a91c8375412cf8480bca81ab6fa6325b1
nebiros88 <nebiros1988@gmail.com>
2025-11-05 16:30:08 +0100

    End of section 8: add new tables - members and photos, create migration, populate the database on app run, add IMemberRepository interface and use repository pattern, prevents navigation properties from sereliazation in responses

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace API.Entities;
+
+public class Photo
+{
+  public int Id { get; set; }
+  public required string Url { get; set; }
+  public string? PublicId { get; set; }
+
+  // navigation property
+  [JsonIgnore]
+  public Member Member { get; set; } = null!;
+
+  [JsonIgnore]
+  public string MemberId { get; set; } = null!;
+}
